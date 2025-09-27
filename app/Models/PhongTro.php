184fe5d46747b414f571fc3584f0555adfba93d2
truong180805 <nguyentruong180805@gmail.com>
2025-09-27 15:09:10 +0700

    thêm api phongtro

--- app/Models/PhongTro.php
+++ app/Models/PhongTro.php
@@ -9,4 +9,13 @@ class PhongTro extends Model
 {
     /** @use HasFactory<\Database\Factories\PhongTroFactory> */
     use HasFactory;
+    
+    protected $fillable = [
+        'user_id', 'tieu_de', 'mo_ta', 'gia', 'dia_chi', 'dien_tich', 'so_dien_thoai' 
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
